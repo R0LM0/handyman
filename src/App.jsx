d5031a5e8290494b5c18523690ef14ff9e209649
Rołmö <rolmo92@gmail.com>
2024-06-06 15:35:20 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+        <p className="text-dark animate-blurred-fade-in">
+          Hola Mundo
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
